79defebf3cfe3e215ff1cf84b8cc39186ff79f35
kelvinokumu <oumakelvinokumu@gmail.com>
2023-04-01 13:41:27 +0300

    added dict

---
+++ 04_dsa/01_basics/02_basics.py
@@ -0,0 +1,8 @@
+my_dict = {'1': 'data',
+           '2': 'structure',
+           '3': 'python',
+           '4': 'programming',
+           '5': 'language'}
+
+print(my_dict['1'])
+print(my_dict.keys())  # get all keys for the dict
