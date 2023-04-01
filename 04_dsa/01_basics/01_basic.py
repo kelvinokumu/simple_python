cec3a6327bdc6491c11de82b5bc75ba5fad1ba2f
kelvinokumu <oumakelvinokumu@gmail.com>
2023-04-01 13:41:41 +0300

    made changes

--- 04_dsa/01_basics/01_basic.py
+++ 04_dsa/01_basics/01_basic.py
@@ -11,8 +11,4 @@ state = True
 print(type(state))
 
 # create a dictionary and assign values to it
-my_dict = {'1': 'data',
-           '2': 'structure',
-           '3': 'python',
-           '4': 'programming',
-           '5': 'language'}
+
