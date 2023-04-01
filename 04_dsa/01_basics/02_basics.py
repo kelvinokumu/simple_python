a7e11d5be0bf7b8c22781fe0f89efe869e080212
kelvinokumu <oumakelvinokumu@gmail.com>
2023-04-01 13:44:50 +0300

    added list

--- 04_dsa/01_basics/02_basics.py
+++ 04_dsa/01_basics/02_basics.py
@@ -6,3 +6,12 @@ my_dict = {'1': 'data',
 
 print(my_dict['1'])
 print(my_dict.keys())  # get all keys for the dict
+print(my_dict.values())  # get all values for the dict
+
+
+# list
+a = ['food', 'bus', 'apple', 'queen']
+print(a)
+mylist = [1, "kenya", "nairobi", 8]
+# accessing elements in list.
+print(mylist[1])
