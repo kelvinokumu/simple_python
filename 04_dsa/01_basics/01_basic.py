6fe39eab5357a15d623672483977dbe03c324b94
kelvinokumu <oumakelvinokumu@gmail.com>
2023-04-01 13:35:42 +0300

    started dsa

---
+++ 04_dsa/01_basics/01_basic.py
@@ -0,0 +1,18 @@
+var = 25
+print(type(var))
+
+name = "kelvin"
+print(type(name))
+
+grade = 'A'
+print(type(grade))
+
+state = True
+print(type(state))
+
+# create a dictionary and assign values to it
+my_dict = {'1': 'data',
+           '2': 'structure',
+           '3': 'python',
+           '4': 'programming',
+           '5': 'language'}
