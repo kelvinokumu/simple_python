67c2585d6254d3d3bc83d269524a3c1133210991
kelvinokumu <oumakelvinokumu@gmail.com>
2023-04-14 12:44:49 +0300

    modified stack

--- 04_dsa/02_data_structures/stacks/Stack.py
+++ 04_dsa/02_data_structures/stacks/Stack.py
@@ -37,3 +37,37 @@ class Stack:
             return self.top.data
         else:
             print("Stack is empty")
+
+
+def check_brackets(expression):
+    brackets_stack = Stack()  # The stack class, we defined in previous section.
+    last = ' '
+    for ch in expression:
+        if ch in ('{', '[', '('):
+            brackets_stack.push(ch)
+
+        if ch in ('}', ']', ')'):
+            last = brackets_stack.pop()
+            if last == '{' and ch == '}':
+                continue
+            elif last == '[' and ch == ']':
+                continue
+            elif last == '(' and ch == ')':
+                continue
+            else:
+                return False
+    if brackets_stack.size > 0:
+        return False
+    else:
+        return True
+
+
+sl = (
+    "{(foo)(bar)}[hello](((this)is)a)test",
+    "{(foo)(bar)}[hello](((this)is)atest",
+    "{(foo)(bar)}[hello](((this)is)a)test))"
+)
+
+for s in sl:
+    m = check_brackets(s)
+    print("{}: {}".format(s, m))
