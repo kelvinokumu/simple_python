587a3e203155958b34a075528569d7bc445752b4
kelvinokumu <oumakelvinokumu@gmail.com>
2023-04-15 11:36:27 +0300

    implemented linear search and added comments

--- 04_dsa/03_algorithms/01_search/linear_search.py
+++ 04_dsa/03_algorithms/01_search/linear_search.py
@@ -1,3 +1,8 @@
+# The search operation is used to find out the index position of a given data item in a list of data
+# items. If the searched item is available in the given list of data items, then the search algorithm
+# returns the index position where it is located; otherwise, it returns that the item is not found.
+# Here, the index position is the location of the desired item in the given list.
+
 def search(unordered_list, term):
     for i, item in enumerate(unordered_list):
         if term == unordered_list[i]:
