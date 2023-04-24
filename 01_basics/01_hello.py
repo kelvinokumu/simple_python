9b7dda609c1a38b83ec4ac161c166d11114e5738
kelvinokumu <oumakelvinokumu.com>
2023-04-24 18:18:04 +0300

    variables

--- 01_basics/01_hello.py
+++ 01_basics/01_hello.py
@@ -1,5 +1,9 @@
 print("Hello world")
 
+# variables
 name = input("What is your name ")
-
 print(f"Your name is {name}")
+
+number = int(input("Enter a number "))
+print(f"The number is {number} ")
+
